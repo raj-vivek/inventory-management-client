2180adb6ac1eb6f97b579d341ca0ab4728cfe938
raj-vivek <vivekraj5217@gmail.com>
2024-06-21 17:46:53 +0530

    Moved API URL to env varibles

--- src/utils/newAxiosRequest.ts
+++ src/utils/newAxiosRequest.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const newAxiosRequest = axios.create({
-  baseURL: "http://localhost:8080" + "/api/v1",
-  // timeout: 2000,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 5000,
   // crossDomain: true, // Allow cross-domain requests
 });
 
